55effafa2544785d2f52e93af913957dc6c889c9
Marko Nikolic <nikolic89@hotmail.com>
2014-09-29 01:44:37 +0200

    Added one more ENV

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('homestead', 'nikolic'),
+	'local' => array('homestead', 'nikolic', 'vmi24753.contabo.net'),
 
 ));
 
