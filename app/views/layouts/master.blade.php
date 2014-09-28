305aedfee3eabe9673847d5241f9ea30f0bd3d36
Marko Nikolic <nikolic89@hotmail.com>
2014-09-28 18:34:27 +0200

    Downloaded all content from CDNs

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -7,72 +7,44 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="../../favicon.ico">
-
     <title>QRLoyalty</title>
-
-    <!-- Bootstrap core CSS -->
-   <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="http://getbootstrap.com/examples/cover/cover.css" rel="stylesheet">
-
+    {{ HTML::style('css/bootstrap.min.css') }}
+    {{ HTML::style('css/cover.css') }}
     {{ HTML::style('css/bootstrap-overrides.css') }}
-
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
   </head>
-
   <body>
-
     <div class="site-wrapper">
-
       <div class="site-wrapper-inner">
-
         <div class="cover-container">
-
-    		<div class="masthead clearfix">
-    			<div class="inner">
-    			  <h3 class="masthead-brand"><a href="{{URL::to('/company/home')}}">QRLoyalty</a></h3>
-    			  <ul class="nav masthead-nav">
-    			    <li ><a href="{{URL::to('/company/home')}}">Coffe dream</a></li>
-              <li ><a href="{{URL::to('/logout')}}"><span class="glyphicon glyphicon-off"></span></a></li>
-    			  </ul>
-    			</div>
-          <div class="inner">
-            <ul id="sub-navigation" class="nav nav-tabs nav-justified" role="tablist">
-              <li class="active"><a href="{{URL::to('/company/home')}}">Posalji kod</a></li>
-              <li><a href="{{URL::to('/company/codes')}}">Kodovi</a></li>
-              <li><a href="{{URL::to('/company/gifts')}}">Katalog poklona</a></li>
-            </ul>
-          </div>
-    		</div>
-
-        @yield('content')
-
-        <div class="mastfoot">
-          <div class="inner">
-            <p>Marko Nikolic</p>
+      		<div class="masthead clearfix">
+      			<div class="inner">
+      			  <h3 class="masthead-brand"><a href="{{URL::to('/company/home')}}">QRLoyalty</a></h3>
+      			  <ul class="nav masthead-nav">
+      			    <li ><a href="{{URL::to('/company/home')}}">Coffe dream</a></li>
+                <li ><a href="{{URL::to('/logout')}}"><span class="glyphicon glyphicon-off"></span></a></li>
+      			  </ul>
+      			</div>
+            <div class="inner">
+              <ul id="sub-navigation" class="nav nav-tabs nav-justified" role="tablist">
+                <li class="active"><a href="{{URL::to('/company/home')}}">Posalji kod</a></li>
+                <li><a href="{{URL::to('/company/codes')}}">Kodovi</a></li>
+                <li><a href="{{URL::to('/company/gifts')}}">Katalog poklona</a></li>
+              </ul>
+            </div>
+      		</div>
+          @yield('content')
+          <div class="mastfoot">
+            <div class="inner">
+              <p>Marko Nikolic</p>
+            </div>
           </div>
         </div>
-
-        </div>
-
       </div>
-
     </div>
-
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-   <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-      <!--<script src="../../assets/js/docs.min.js"></script>
-   IE10 viewport hack for Surface/desktop Windows 8 bug
-    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script> -->
+    {{ HTML::script('js/libs/jquery.min.js') }}
+    {{ HTML::script('js/libs/bootstrap.min.js') }}
   </body>
 </html>
