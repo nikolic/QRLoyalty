2e0c37c913ce933f9f56028dc7e7748762390f42
Marko Nikolic <nikolic89@hotmail.com>
2014-09-28 18:59:21 +0200

    Added config

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -23,9 +23,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'homestead',
-			'username'  => 'homestead',
-			'password'  => 'secret',
+			'database'  => 'qrl',
+			'username'  => 'root',
+			'password'  => 'qwe123',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
