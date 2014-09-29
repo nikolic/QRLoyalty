c7e80fcb640e9abac429719ed091e8ece4dce9f8
Marko Nikolic <nikolic89@hotmail.com>
2014-09-30 01:22:10 +0200

    Mockup for customer created.

--- app/views/customer/account.blade.php
+++ app/views/customer/account.blade.php
@@ -4,10 +4,36 @@
 	<div class="col-md-12">
 		<div class="panel panel-custom">
 	      <div class="panel-heading">
-	        <h3 class="panel-title">User codes.</h3>
+	        <h3 class="panel-title">Marko Nikolic (nikolic89@hotmail.com)</h3>
 	      </div>
 			<div class="panel-body">
-				account
+			    <div class="list-group" id="codes">
+			        <div class="list-group-item">
+			            <p class="list-group-item-text">Your Name: Marko Nikolic</p>
+			            <p class="list-group-item-text">Email: nikolic89@hotmail.com</p>
+			            <p class="list-group-item-text">Created: 20.12.2014</p>
+			            <p class="list-group-item-text">Status: <span class="label label-success">Activan</span></p>
+			        </div>
+			        <div class="list-group-item">
+			            <p class="list-group-item-text margin-bottom">
+			            	Change password
+			            </p>
+			            <p class="list-group-item-text">
+			            	<div class="row">
+				            	<div class="col-md-6">
+				            		<input type="password" class="form-control" placeholder="New password" required="">
+				            	</div>
+				            	<div class="col-md-6">
+				            		<input type="password" class="form-control" placeholder="New password confirmation" required="">
+				            	</div>
+			            	</div>
+			            </p>
+			            <h4 class="list-group-item-text"><span class="label label-info">Change password</span></h4>
+			        </div>
+			        <div class="list-group-item">
+			            <h4 class="list-group-item-heading"><span class="label label-default">Deactivate account</span></h4>
+			        </div>
+			    </div>
 			</div>
 		</div>
 	</div>
--- app/views/customer/catalogue.blade.php
+++ app/views/customer/catalogue.blade.php
@@ -4,10 +4,43 @@
 	<div class="col-md-12">
 		<div class="panel panel-custom">
 	      <div class="panel-heading">
-	        <h3 class="panel-title">User codes.</h3>
+			<div class="row">
+				<div class="col-md-12">
+					<select class="form-control">
+						<option>Sve</option>
+						<option>Coffee dream</option>
+						<option>Costa</option>
+					</select>	
+				</div>
+			</div>
 	      </div>
 			<div class="panel-body">
-				catalogue
+			    <div class="list-group" id="codes">
+			        <a href="#" class="list-group-item">
+			        	<div class="list-group-item-image">
+			        		<span class="glyphicon glyphicon-gift"></span>
+			           </div>
+			            <h4 class="list-group-item-heading"> 2 litra kokakole </h4>
+			            <p class="list-group-item-text">Cena: 12 (QRL bodova)</p>
+			            <p class="list-group-item-text"><span class="label label-success">Mozete preuzeti poklon</span></p>
+			        </a>
+			        <a href="#" class="list-group-item">
+			        	<div class="list-group-item-image">
+			        		<span class="glyphicon glyphicon-gift"></span>
+			           </div>
+			            <h4 class="list-group-item-heading"> 2 litra kokakole </h4>
+			            <p class="list-group-item-text">Cena: 12 (QRL bodova)</p>
+			            <p class="list-group-item-text"><span class="label label-default">Nemate dovoljno kodova</span></p>
+			        </a>
+			        <a href="#" class="list-group-item">
+			        	<div class="list-group-item-image">
+			        		<span class="glyphicon glyphicon-gift"></span>
+			           </div>
+			            <h4 class="list-group-item-heading"> 2 litra kokakole </h4>
+			            <p class="list-group-item-text">Cena: 12 (QRL bodova)</p>
+			            <p class="list-group-item-text"><span class="label label-success">Mozete preuzeti poklon</span></p>
+			        </a>
+			    </div>
 			</div>
 		</div>
 	</div>
