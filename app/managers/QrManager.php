ebf94c7dd21cfc0342448f0589cd3b68d894b573
Marko Nikolic <nikolic89@hotmail.com>
2014-10-01 00:30:57 +0200

    Added QRCode lib.

---
+++ app/managers/QrManager.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Log;
+
+class QRManager {
+	
+	public static function generateCode()
+	{
+		Log::debug('Executing RoleManager@getRole', array('role' => $role));
+		
+
+		return false;
+	}
+
+	public static function insertCodeInDB()
+	{
+		Log::debug('Executing RoleManager@getRole', array('role' => $role));
+		
+
+		return false;
+	}
+}
