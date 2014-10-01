98a68f68761be0c7037cd16f1353e239f69a882b
Marko Nikolic <nikolic89@hotmail.com>
2014-10-01 17:28:26 +0200

    Added QR on index page.

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -5,7 +5,7 @@
         <h1 class="cover-heading">Loyalty programs</h1>
         <p class="lead"> are structured marketing efforts that reward, and therefore encourage, loyal buying behavior – behavior which is potentially beneficial to the firm.</p>
         <p class="lead">
-        	
+          <img src="data:image/png;base64, {{ base64_encode(QrCode::format('png')->size(100)->color(81,175,255)->backgroundColor(51,51,51)->generate('QRLoyalty')); }} ">
         </p>
       </div>
 @stop
